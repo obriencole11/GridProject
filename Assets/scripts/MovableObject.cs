c75d14760849437e25e87d0e31e1ba1748aab615
obriencole11 <obriencole11@gmail.com>
2019-01-25 01:52:40 -0800

    Added initial files

---
+++ Assets/scripts/MovableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BaseObject))]
+[RequireComponent(typeof(GridObject))]
+public class MovableObject : MonoBehaviour
+{
+
+    public GridObject gridObject;
+    public BaseObject baseObject;
+
+    protected void Awake()
+    {
+        baseObject = GetComponent<BaseObject>();
+        gridObject = GetComponent<GridObject>();
+        GameManager.Instance.movableObjects.Add(this);
+    }
+}
---
+++ Assets/scripts/PlayerObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MovableObject
+{
+    void Awake()
+    {
+        base.Awake();
+        GameManager.Instance.playerObjects.Add(this);
+    }
+}
